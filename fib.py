bf5304e3f1681b05e41f106ad5ed466bd37fe3a5
Cesar Enrique Garcia Dabo <cgarcia@eso.org>
2018-06-04 11:43:35 +0200

    First implementation. Tested and working

--- fib.py
+++ fib.py
@@ -1,1 +1,10 @@
-test
+def fib(N):
+    if N == 0:
+        return 1
+    if N == 1:
+        return 1
+    return fib(N - 1) + fib(N - 2)
+
+if __name__ == '__main__':
+    for i in range(5):
+        print fib(i)
